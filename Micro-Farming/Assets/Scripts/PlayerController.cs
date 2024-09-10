ea319402d09904bd0bf14f47ccc0a749f85d31f8
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-09-10 11:29:08 -0300

    Player: Move
    PlayerController Properties
    PlayerController InputAction Callbacks

--- Micro-Farming/Assets/Scripts/PlayerController.cs
+++ Micro-Farming/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public float moveSpeed;
+   
+   private Vector2 moveInput;
+   private bool interactInput;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private Vector2 facingDir;
+
+   public LayerMask interactLayerMask;
+
+   public Rigidbody2D rig;
+   public SpriteRenderer sr;
+
+   private void FixedUpdate()
+   {
+      rig.velocity = moveInput.normalized * moveSpeed;
+   }
+   
+   public void OnMoveInput(InputAction.CallbackContext context)
+   {
+      moveInput = context.ReadValue<Vector2>();
+   }
+   
+   public void OnInteractInput(InputAction.CallbackContext context)
+   {
+      if (context.phase == InputActionPhase.Performed)
+      {
+         interactInput = true;
+      }
+   }
 }
