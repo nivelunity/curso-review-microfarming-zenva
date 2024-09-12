aa6897ac614262bdad9625db0641b3ea85780fd7
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-09-12 11:22:52 -0300

    Crop Script

---
+++ Micro-Farming/Assets/Scripts/Crop.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crop : MonoBehaviour
+{
+   private CropData curCrop;
+   private int plantDay;
+   private int daySinceLastWatered;
+
+   public SpriteRenderer sr;
+}
