3fda5842ba63cfb455edffaa1fbc304416bc1041
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-09-12 11:01:58 -0300

    CropData Script

---
+++ Micro-Farming/Assets/Scripts/CropData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CropData : MonoBehaviour
+{
+   public int daysToGrow;
+   public Sprite[] growProgressSprites;
+   public Sprite readyToHavestSprite;
+
+   public int purchasePrice;
+   public int sellprice;
+}
