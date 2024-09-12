7245e98a86a882d77b1824f2e1d1fbe2156866c8
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-09-12 11:27:17 -0300

    Crop Desing

--- Micro-Farming/Assets/Scripts/Crop.cs
+++ Micro-Farming/Assets/Scripts/Crop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Crop : MonoBehaviour
 {
@@ -9,4 +10,42 @@ public class Crop : MonoBehaviour
    private int daySinceLastWatered;
 
    public SpriteRenderer sr;
+
+   public static event UnityAction<CropData> onPlantCrop;
+   public static event UnityAction<CropData> onHarvestCrop;
+
+   public void Plant(CropData crop)
+   {
+      
+   }
+
+   public void NewDayCheck()
+   {
+      
+   }
+   
+   public void UpdateCropSprite()
+   {
+      
+   }
+
+   public void Water()
+   {
+      
+   }
+
+   public void Harvest()
+   {
+      
+   }
+
+   int CropProgress()
+   {
+      return GameManager.instance.curDay - plantDay;
+   }
+
+   public bool CanHarvest()
+   {
+      return CropProgress() >= curCrop.daysToGrow;
+   }
 }
