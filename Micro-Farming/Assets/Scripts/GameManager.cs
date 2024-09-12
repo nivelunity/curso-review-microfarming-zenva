46700d5e7630b474bb7bc636be352af663ed9537
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-09-12 11:18:43 -0300

    GameManager Desing

--- Micro-Farming/Assets/Scripts/GameManager.cs
+++ Micro-Farming/Assets/Scripts/GameManager.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public int curDay;
     public int money;
+    public int cropInventory;
 
     public CropData selectedCropToPlant;
+
+    public event UnityAction onNewDay;
     
     //Singleton
     public static GameManager instance;
@@ -24,5 +28,40 @@ public class GameManager : MonoBehaviour
             instance = this;
         }
             
+    }
+
+    public void SetNextDay()
+    {
+        
+    }
+
+    public void OnPlantCrop(CropData crop)
+    {
+        
+    }
+
+    public void OnHarvestCrop(CropData crop)
+    {
+        
+    }
+
+    public void PurchaseCrot(CropData crop)
+    {
+        
+    }
+
+    public bool CanPlantCrop()
+    {
+        return true;
+    }
+
+    public void OnBuyCropButton(CropData crop)
+    {
+        
+    }
+
+    void UpdateStatesText()
+    {
+        
     }
 }
