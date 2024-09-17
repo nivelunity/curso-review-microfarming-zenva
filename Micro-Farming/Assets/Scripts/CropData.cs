0b04f300dc072ebcf6e1f9a0779135d8a03daad4
Gaston Caminiti <gastoncaminiti@outlook.com>
2024-09-17 10:44:43 -0300

    Header

--- Micro-Farming/Assets/Scripts/CropData.cs
+++ Micro-Farming/Assets/Scripts/CropData.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Crop Data", menuName = "New Crop Data")]
 public class CropData : ScriptableObject
 {
+   [Header("1. Configuracion de Crecimientos \n")]
    public int daysToGrow;
    public Sprite[] growProgressSprites;
    public Sprite readyToHarvestSprite;
-
-   public int purchasePrice;
-   public int sellPrice;
+   
+   [Header("2. Configuracion de Comercio \n")]
+   [Range(1,3000)] public int purchasePrice;
+   [Range(1,3000)] public int sellPrice;
 }
